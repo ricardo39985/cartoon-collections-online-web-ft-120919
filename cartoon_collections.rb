e4db173785ba7d1827f44080b0386615571fb4ef
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-13 01:14:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,6 @@
 def roll_call_dwarves(array_of_dwarves)# code an argument here
   # Your code here
-  array.each_with_index
-    |dwarf, index| puts "#{index+1} #{dwarf}"
-  end
+  array_of_dwarves.each_with_index {|dwarf, index| puts "#{index+1} #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
