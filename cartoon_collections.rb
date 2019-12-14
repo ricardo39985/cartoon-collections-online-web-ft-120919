4592a7f85e1ea11a9c597784ea9314b425c0afd5
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 15:52:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
 def find_the_cheese(array_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  # matches = cheese_types & array_cheese
-  [cheese_types & array_cheese].length > 0 ?  [cheese_types & array_cheese][0] : nil
+  matches = cheese_types & array_cheese
+  matches.length > 0 ?  matches[0] : nil
   # if matches.length >0
   #   return matches[0]
   # else
